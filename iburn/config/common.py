022759aa24d7d175fae48f460f6829bbf9ab44b9
Stephen Suffian <stephen.suffian@gmail.com>
2017-07-27 22:13:04 -0600

    Updated STATIC_ROOT reference

--- iburn/config/common.py
+++ iburn/config/common.py
@@ -55,6 +55,7 @@ class Common(Configuration):
 
     ADMINS = (
         ('Author', 'chrisballinger@gmail.com'),
+        ('Author', 'stephensuffian@gmail.com'),
     )
 
     # Postgres
@@ -72,8 +73,17 @@ class Common(Configuration):
     LOGIN_REDIRECT_URL = '/'
 
    # https://docs.djangoproject.com/en/1.9/howto/static-files/
-    STATIC_URL = '/static/'
 
+    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/1.9/howto/static-files/
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+
+
+    STATIC_URL = '/static/'
+    #STATIC_ROOT = join(os.path.dirname(BASE_DIR), 'staticfiles')
     # Additional locations of static files
     STATICFILES_DIRS = (
         # Put strings here, like "/home/html/static" or "C:/www/django/static".
